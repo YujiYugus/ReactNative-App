5a848396380c2fc5d966f12b7a95826a91bbf0cc
YujiYugus <yujiejulianasuguy@gmail.com>
2025-09-17 09:04:40 -0300

    criando AuthContext, aplicando modal nas duas telas de list e user

--- src/components/CustomTabBar/index.tsx
+++ src/components/CustomTabBar/index.tsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
 import { AntDesign, FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
 import { themas } from "../../global/themes";
+import { AuthContextList } from "../../context/authContext_list";
 
 export default ({ state, navigation }: any) => {
+
+    const { onOpen } = useContext<any>(AuthContextList)
 
     const go = (screenName: string) => {
         navigation.navigate(screenName)
     }
     return (
         <View style={style.tabArea}>
-            <TouchableOpacity style={style.tabItem} onPress={()=>go ("List")}>
+            <TouchableOpacity style={style.tabItem} onPress={() => go("List")}>
                 <AntDesign
                     name="bars"
-                    style={{ opacity: state.index === 0 ? 1: 0.3,
-                        color:themas.colors.primary, 
-                        fontSize: 32 
+                    style={{
+                        opacity: state.index === 0 ? 1 : 0.3,
+                        color: themas.colors.primary,
+                        fontSize: 32
                     }}
                 />
             </TouchableOpacity>
-            <TouchableOpacity style={style.tabItemButton}>
+            <TouchableOpacity style={style.tabItemButton} onPress={() => onOpen()}>
                 <View style={{ width: '100%', left: 10, top: 4 }}>
                     <Entypo
                         name="plus"
@@ -37,12 +41,13 @@ export default ({ state, navigation }: any) => {
                     />
                 </View>
             </TouchableOpacity>
-            <TouchableOpacity style={style.tabItem} onPress={()=>go ("User")}>
+            <TouchableOpacity style={style.tabItem} onPress={() => go("User")}>
                 <FontAwesome
                     name="user"
-                    style={{ opacity: state.index === 1 ? 1: 0.3,
-                        color:themas.colors.primary, 
-                        fontSize: 32 
+                    style={{
+                        opacity: state.index === 1 ? 1 : 0.3,
+                        color: themas.colors.primary,
+                        fontSize: 32
                     }}
                 />
             </TouchableOpacity>
