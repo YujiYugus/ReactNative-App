88c7c2924ad3419a42b09eea4057b1f890430c73
ARNON �UJI SUGU� <arnon.8980@aluno.pr.senac.br>
2025-09-29 12:11:50 -0300

    Criando dateTimePicker, funcionalidade para o modal, alem de criar outros componentes e constantes par apicker, estilizando o modal e o custom date time picker, mudancas nas cores dos temas do app

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -5,6 +5,7 @@ import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
 import { themas } from "../global/themes";
 import { Flag } from "../components/Flag";
+import CustomDateTimePicker from "../components/CustomDateTimePicker";
 
 export const AuthContextList: any = createContext({});
 
@@ -90,9 +91,15 @@ export const AuthProviderList = (props: any): any => {
                     />
                 </View>
                 <View style={{ width: '40%' }}>
-                    <Input
+                    {/* <Input
                         title="Tempo limite:"
                         labelStyle={styles.label}
+                    /> */}
+                    <CustomDateTimePicker 
+                    onDateChange={() => { }}
+                    setShow={() => { }}
+                    show={true}
+                    type={'date'}
                     />
                 </View>
                 <View style={styles.containerFlag}>
