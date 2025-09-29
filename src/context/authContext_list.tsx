adec3fb75c2f51430aae0cc506af85e148a75cc7
ARNON �UJI SUGU� <arnon.8980@aluno.pr.senac.br>
2025-09-29 09:01:29 -0300

    Criando constantes que usaremos para os proximos passos da programacao, alem de deixar o teclado responsivo quando abrir em qualquer aparelho celular

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useContext, useRef, useEffect } from "react";
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { createContext, useContext, useRef, useEffect, useState } from "react";
+import { Dimensions, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
@@ -16,6 +16,12 @@ const flags = [
 export const AuthProviderList = (props: any): any => {
 
     const modalizeRef = useRef<Modalize>(null);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [selectedFlag, setSelected] = useState('Urgente');
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [selectedTime, setSelectedTime] = useState(new Date());
+
     const onOpen = () => {
         modalizeRef?.current?.open();
 
@@ -35,7 +41,7 @@ export const AuthProviderList = (props: any): any => {
                     <Flag
                         caption={item.caption}
                         color={item.color}
-                        selected
+                    selected
                     />
                 </TouchableOpacity>
             ))
@@ -44,7 +50,10 @@ export const AuthProviderList = (props: any): any => {
 
     const _container = () => {
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
                 <View style={styles.header}>
                     <TouchableOpacity onPress={() => onClose()}>
                         <MaterialIcons
@@ -67,6 +76,8 @@ export const AuthProviderList = (props: any): any => {
                     <Input
                         title="Titulo"
                         labelStyle={styles.label}
+                        value={title}
+                        onChangeText={setTitle}
                     />
                     <Input
                         title="Descrição"
@@ -74,6 +85,8 @@ export const AuthProviderList = (props: any): any => {
                         height={100}
                         multiline
                         numberOfLines={5}
+                        value={description}
+                        onChangeText={setDescription}
                     />
                 </View>
                 <View style={{ width: '40%' }}>
@@ -84,11 +97,11 @@ export const AuthProviderList = (props: any): any => {
                 </View>
                 <View style={styles.containerFlag}>
                     <Text style={styles.label}>Flags:</Text>
-                     <View style={styles.rowFlags}>
+                    <View style={styles.rowFlags}>
                         {_renderFlags()}
                     </View>
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         )
     }
     return (
